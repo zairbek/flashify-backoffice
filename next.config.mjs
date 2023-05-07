7a7abdc6d00bfaa8fd43031389cf1e0dfc82bead
Zair <nurmukhamadov.z@future-group.ru>
2023-05-07 15:49:16 +0600

    init

--- next.config.mjs
+++ next.config.mjs
@@ -2,17 +2,6 @@
  * @type {import('next').NextConfig}
  */
  const nextConfig = {
-  basePath: "/admin-one-react-tailwind",
-  async redirects() {
-    return [
-      {
-          source: '/',
-          destination: '/admin-one-react-tailwind',
-          basePath: false,
-          permanent: false
-      }
-    ]
-  },
   images: {
     unoptimized: true,
     remotePatterns: [
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,76 +1,64 @@
+import React from 'react'
+import type { ReactElement } from 'react'
 import Head from 'next/head'
-import Image from 'next/image'
-import { useRouter } from 'next/router'
-import React, { ReactElement } from 'react'
+import BaseButton from '../components/BaseButton'
 import CardBox from '../components/CardBox'
+import SectionFullScreen from '../components/SectionFullScreen'
 import LayoutGuest from '../layouts/Guest'
-import SectionMain from '../components/SectionMain'
-import { StyleKey } from '../interfaces'
-import { gradientBgPurplePink } from '../colors'
-import { appTitle } from '../config'
-import { useAppDispatch } from '../stores/hooks'
-import { setDarkMode, setStyle } from '../stores/styleSlice'
+import { Field, Form, Formik } from 'formik'
+import FormField from '../components/FormField'
+import FormCheckRadio from '../components/FormCheckRadio'
+import BaseDivider from '../components/BaseDivider'
+import BaseButtons from '../components/BaseButtons'
+import { useRouter } from 'next/router'
+import { getPageTitle } from '../config'
 
-const StyleSelect = () => {
-  const dispatch = useAppDispatch()
-
-  dispatch(setDarkMode(false))
-
-  const styles: StyleKey[] = ['white', 'basic']
-
+export default function Error() {
   const router = useRouter()
 
-  const handleStylePick = (e: React.MouseEvent, style: StyleKey) => {
-    e.preventDefault()
-
-    dispatch(setStyle(style))
-
+  const handleSubmit = () => {
     router.push('/dashboard')
   }
 
   return (
     <>
       <Head>
-        <title>{appTitle}</title>
+        <title>{getPageTitle('Login')}</title>
       </Head>
-      <div className={`flex min-h-screen items-center justify-center ${gradientBgPurplePink}`}>
-        <SectionMain>
-          <h1 className="text-4xl md:text-5xl text-center text-white font-bold mt-12 mb-3 lg:mt-0">
-            Pick a style&hellip;
-          </h1>
-          <h2 className="text-xl md:text-xl text-center text-white mb-12">
-            Style switching with a single{' '}
-            <code className="px-1.5 py-0.5 rounded bg-white bg-opacity-20">action()</code>
-          </h2>
-          <div className="grid gap-6 grid-cols-1 lg:grid-cols-2 px-6 max-w-6xl mx-auto">
-            {styles.map((style) => (
-              <CardBox
-                key={style}
-                className="cursor-pointer bg-gray-50"
-                isHoverable
-                onClick={(e) => handleStylePick(e, style)}
-              >
-                <div className="mb-3 md:mb-6">
-                  <Image
-                    src={`https://static.justboil.me/templates/one/small/${style}-v3.png`}
-                    width={1280}
-                    height={720}
-                    alt={style}
-                  />
-                </div>
-                <h1 className="text-xl md:text-2xl font-black capitalize">{style}</h1>
-                <h2 className="text-lg md:text-xl">& Dark mode</h2>
-              </CardBox>
-            ))}
-          </div>
-        </SectionMain>
-      </div>
+
+      <SectionFullScreen bg="purplePink">
+        <CardBox className="w-11/12 md:w-7/12 lg:w-6/12 xl:w-4/12 shadow-2xl">
+          <Formik
+            initialValues={{ login: 'john.doe', password: 'bG1sL9eQ1uD2sK3b', remember: true }}
+            onSubmit={() => handleSubmit()}
+          >
+            <Form>
+              <FormField label="Login" help="Please enter your login">
+                <Field name="login" />
+              </FormField>
+
+              <FormField label="Password" help="Please enter your password">
+                <Field name="password" type="password" />
+              </FormField>
+
+              <FormCheckRadio type="checkbox" label="Remember">
+                <Field type="checkbox" name="remember" />
+              </FormCheckRadio>
+
+              <BaseDivider />
+
+              <BaseButtons>
+                <BaseButton type="submit" label="Login" color="info" />
+                <BaseButton href="/dashboard" label="Home" color="info" outline />
+              </BaseButtons>
+            </Form>
+          </Formik>
+        </CardBox>
+      </SectionFullScreen>
     </>
   )
 }
 
-StyleSelect.getLayout = function getLayout(page: ReactElement) {
+Error.getLayout = function getLayout(page: ReactElement) {
   return <LayoutGuest>{page}</LayoutGuest>
 }
-
-export default StyleSelect
