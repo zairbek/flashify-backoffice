d61f75a3db6ed77b529c93a2c83024746b72a845
Zair <nurmukhamadov.z@future-group.ru>
2023-05-18 09:45:08 +0600

    .

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -30,5 +30,5 @@ export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 export const useStoreDispatch = () => useDispatch<typeof store.dispatch>()
 export const wrapper = createWrapper<RootStore>(makeStore, {
-  debug: process.env.NODE_ENV === 'development'
+  // debug: process.env.NODE_ENV === 'development'
 });
